8c0db51ab8a90f2be3cef2a0fdea46a39478dfde
robin widjaja <robinsalim@yahoo.com>
2020-04-16 15:24:24 +0700

    selesai fungsi game-page

---
+++ src/views/Game.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+      <question />
+  </div>
+</template>
+
+<script>
+import question from '@/components/Question'
+
+export default {
+  name: 'GamePage',
+  components: {
+    question
+  }
+}
+</script>
+
+<style>
+#question {
+    background-color: red;
+}
+
+#answers {
+    background-color: blue;
+}
+
+#a {
+    background-color: yellow;
+}
+
+#b {
+    background-color: green;
+}
+
+#c {
+    background-color: orange;
+}
+
+#d {
+    background-color: aqua;
+}
+</style>
