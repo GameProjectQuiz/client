6a5da395e4cc2230150259408c12056a2814b471
hariszaki17 <hariszaki17@gmail.com>
2020-04-16 15:01:20 +0700

    lobby finished

---
+++ src/views/LobbyPage.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="LobbyPage d-flex flex-column align-items-center justify-content-center">
+      <h1>LOBBY</h1>
+      <div class="board bg-light p-2 d-flex flex-wrap align-items-start justify-content-center" style="overflow: scroll;">
+          <PlayerCard v-for="player in players" :key="player.id" :name="player.name" :status="player.status" :id="player.id"></PlayerCard>
+      </div>
+  </div>
+</template>
+
+<script>
+import PlayerCard from '../components/PlayerCard'
+export default {
+    name: 'LobbyPage',
+    components: {
+        PlayerCard
+    },
+    computed: {
+        players() {
+            return this.$store.state.player
+        }
+    },
+    created() {
+    }
+}
+</script>
+
+<style>
+@keyframes bgcolor {
+    0% {
+        background-color: #45a3e5
+    }
+
+    30% {
+        background-color: #66bf39
+    }
+
+    60% {
+        background-color: #eb670f
+    }
+
+    90% {
+        background-color: #f35
+    }
+
+    100% {
+        background-color: #864cbf
+    }
+}
+
+.LobbyPage {
+    width: 100vw;
+    height: 100vh;
+    -webkit-animation: bgcolor 20s infinite;
+    animation: bgcolor 10s infinite;
+    -webkit-animation-direction: alternate;
+    animation-direction: alternate;
+}
+.board {
+    width: 90%;
+    height: 80%;
+    border-radius: 10px;
+    overflow: scroll;
+}
+
+h1 {
+    font-size: 50px;
+    font-weight: 500;
+}
+</style>
